ddf9de3c8c66c51b6a8da6193c29c2722db6e1dc
EdgarRetes <edgar.retes24@gmail.com>
2025-09-06 14:06:30 -0600

    business rule add logic

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
---
+++ src/native_files/native_files.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { NativeFileService } from './native_files.service';
+import { NativeFileController } from './native_files.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [NativeFileController],
+  providers: [NativeFileService],
+})
+export class RulesModule {}
